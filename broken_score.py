22a69794ab506d01124e7f3ba59ba18080da1a74
Yue Cao <yue.cao@my.jcu.edu.au>
2021-11-30 00:52:09 +0800

    Add a main function

--- broken_score.py
+++ broken_score.py
@@ -1,9 +1,13 @@
-score = float(input("Enter score: "))
-if 90 <= score <= 100:
-    print("Your score is excellent.")
-elif 50 <= score < 90:
-    print("Your score is pass.")
-elif 0 <= score < 50:
-    print("Your score is bad.")
-else:
-    print("Invalid score")
+def main():
+    """Get scores and determine grades"""
+    score = float(input("Enter score: "))
+    grade = determine_grade(score)
+
+    if 90 <= score <= 100:
+        print("Your score is excellent.")
+    elif 50 <= score < 90:
+        print("Your score is pass.")
+    elif 0 <= score < 50:
+        print("Your score is bad.")
+    else:
+        print("Invalid score")
